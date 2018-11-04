595ce271e197a65bf643d32c8e0e780ec42700be
Dimas <myemail@email.com>
2018-11-04 15:04:06 +0300

    some changes

--- src/main/java/structural/adapter/main.java
+++ src/main/java/structural/adapter/main.java
@@ -1,7 +1,13 @@
 package structural.adapter;
 
+/**
+ * Simple fun example of Adapter pattern usage
+ * Trying to use RadioactiveOrb as power source for CarEngine
+ * with EnergyAdapter
+ */
 public class main {
     public static void main(String[] args) {
+
         RadioactiveOrb orb = new RadioactiveOrb(10, 100);
         EmissionEnergy emissionEnergy = orb.emissionEnergy();
         ElectricEnergy electricEnergy = EnergyAdapter.transform(emissionEnergy);

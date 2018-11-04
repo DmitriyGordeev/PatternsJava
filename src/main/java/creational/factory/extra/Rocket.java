7eedb4a90c4295f6ecc5b88fd875896af2cb7269
Dimas <myemail@email.com>
2018-11-04 12:43:54 +0300

    builder attached

--- src/main/java/creational/factory/extra/Rocket.java
+++ src/main/java/creational/factory/extra/Rocket.java
@@ -30,4 +30,67 @@ public class Rocket {
         this.launchStartTime = launchStartTime;
     }
 
+    public static class Builder {
+        private float size;
+        private int   stageNumber;
+        private float startPower;
+        private float maxVelocity;
+        private String manufacturerName;
+        private String spaceCenterName;
+        private String destination;
+        private String launchStartTime;
+
+        public Builder setStartPower(float startPower) {
+            this.startPower = startPower;
+            return this;
+        }
+
+        public Builder setStageNumber(int stageNumber) {
+            this.stageNumber = stageNumber;
+            return this;
+        }
+
+        public Builder setSpaceCenterName(String spaceCenterName) {
+            this.spaceCenterName = spaceCenterName;
+            return this;
+        }
+
+        public Builder setSize(float size) {
+            this.size = size;
+            return this;
+        }
+
+        public Builder setMaxVelocity(float maxVelocity) {
+            this.maxVelocity = maxVelocity;
+            return this;
+        }
+
+        public Builder setManufacturerName(String manufacturerName) {
+            this.manufacturerName = manufacturerName;
+            return this;
+        }
+
+        public Builder setLaunchStartTime(String launchStartTime) {
+            this.launchStartTime = launchStartTime;
+            return this;
+        }
+
+        public Builder setDestination(String destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        public Rocket build() {
+            return new Rocket(
+                    this.size,
+                    this.stageNumber,
+                    this.startPower,
+                    this.maxVelocity,
+                    this.manufacturerName,
+                    this.spaceCenterName,
+                    this.destination,
+                    this.launchStartTime);
+        }
+    }
+
 }
